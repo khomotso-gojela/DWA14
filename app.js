fad1559641d2fc41335590429452c97b8b1b05ec
khomotso gojela <motsoak49@gmail.com>
2023-11-01 12:20:44 +0200

    using state's alert property

--- app.js
+++ app.js
@@ -4,16 +4,10 @@ import { getState } from "./modules/store.js";
 
 export const App = (state) => {
 
-    let open = false
-
-    if (state.alert) {
-        open = true
-    }
-
     return html`
     <div class='wrapper' data-wrapper>
         <div class="alert" >
-            <sl-alert ?open=${open} data-alert >
+            <sl-alert ?open=${state.alert} data-alert >
                 <sl-icon slot="icon" name="info-circle"></sl-icon>
                 COUNTER HAS BEEN RESET
             </sl-alert>
